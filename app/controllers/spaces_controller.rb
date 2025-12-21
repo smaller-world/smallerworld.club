7e3b8dc07aee92e10241e940e4f6e4a3daa35e3c
kai <hulloitskai@gmail.com>
2025-12-21 04:25:57 -0500

    Should refresh after post update instead of recede

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -97,7 +97,7 @@ class SpacesController < ApplicationController
         authorize!(@space)
         space_params = params.expect(space: permitted_space_attributes)
         if @space.update(space_params)
-          recede_or_redirect_to(
+          refresh_or_redirect_to(
             space_path(@space, emulate_native_app: 1),
             status: :see_other,
           )
