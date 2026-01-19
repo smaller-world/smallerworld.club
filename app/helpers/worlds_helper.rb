8cb4443793811ae3000ca62de028622dc11ef61e
kai <hulloitskai@gmail.com>
2026-01-19 15:13:05 -0500

    Consistently route to /world from cards, universe, etc.

---
+++ app/helpers/worlds_helper.rb
@@ -0,0 +1,26 @@
+# typed: true
+# frozen_string_literal: true
+
+module WorldsHelper
+  extend T::Sig
+  extend T::Helpers
+
+  requires_ancestor { ActionView::Base }
+
+  # == Methods ==
+
+  sig do
+    params(
+      world: World,
+      friend_token: T.nilable(String),
+      options: T.untyped,
+    ).returns(String)
+  end
+  def user_aware_world_path(world, friend_token: nil, **options)
+    if allowed_to?(:manage?, world)
+      user_world_path(**options)
+    else
+      world_path(world, friend_token:, **options)
+    end
+  end
+end
