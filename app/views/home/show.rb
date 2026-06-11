6737b319ce56c08015b1e47b5df9f0488b054702
kai <1376678+hulloitskai@users.noreply.github.com>
2026-06-11 12:17:01 -0400

    Fix home layout

--- app/views/home/show.rb
+++ app/views/home/show.rb
@@ -25,8 +25,10 @@ class Views::Home::Show < Views::Base
   sig { override.void }
   def view_template
     Components::AppLayout(display_header: true) do |layout|
-      layout.page_container(class: "flex-1 max-w-lg flex flex-col gap-8") do
-        unless hotwire_native_app?
+      show_alert = !hotwire_native_app?
+
+      layout.page_container(class: "flex-1 max-w-lg flex flex-col gap-6") do
+        if show_alert
           Components::Alert(class: "gap-y-1") do |alert|
             alert.title do
               "please try our app!!"
@@ -45,124 +47,141 @@ class Views::Home::Show < Views::Base
           end
         end
 
-        turbo_frame_tag(
-          :your_worlds,
-          target: "_top",
-          class: "flex gap-6 flex-wrap justify-center",
-        ) do
-          @current_user.owned_worlds.each do |world|
-            link_to(world, class: "world-icon-container hover:underline") do
-              image_tag(world.page_icon_variant, class: "world-icon")
-              span(class: "world-icon-label") do
-                world.name
-              end
-            end
-          end
-
-          if @current_user.owned_worlds.empty?
-            link_to(new_world_path, class: "world-icon-container hover:underline") do
-              Components::Button(
-                element: :div,
-                variant: :outline,
-                class: "home-world-icon border-dashed",
-              ) do
-                image_tag("logo.png", class: "size-12")
-              end
-              span(class: "world-icon-label") do
-                "create your world"
-              end
-            end
-          end
+        div(class: "flex-1 flex flex-col gap-8 justify-center") do
+          your_worlds
+          other_worlds
         end
 
-        turbo_frame_tag(
-          :other_worlds,
-          class: "flex gap-4 flex-wrap justify-center",
-          target: "_top",
+        if show_alert
+          div(class: "flex-1 max-h-36 min-h-0 shrink")
+        end
+      end
+    end
+  end
+
+  private
+
+  sig { void }
+  def your_worlds
+    turbo_frame_tag(
+      :your_worlds,
+      target: "_top",
+      class: "flex gap-6 flex-wrap justify-center",
+    ) do
+      @current_user.owned_worlds.each do |world|
+        link_to(world, class: "world-icon-container hover:underline") do
+          image_tag(world.page_icon_variant, class: "world-icon")
+          span(class: "world-icon-label") do
+            world.name
+          end
+        end
+      end
+
+      if @current_user.owned_worlds.empty?
+        link_to(new_world_path, class: "world-icon-container hover:underline") do
+          Components::Button(
+            element: :div,
+            variant: :outline,
+            class: "home-world-icon border-dashed",
+          ) do
+            image_tag("logo.png", class: "size-12")
+          end
+          span(class: "world-icon-label") do
+            "create your world"
+          end
+        end
+      end
+    end
+  end
+
+  sig { void }
+  def other_worlds
+    turbo_frame_tag(
+      :other_worlds,
+      class: "flex gap-4 flex-wrap justify-center",
+      target: "_top",
+    ) do
+      @accessible_worlds.each do |world|
+        link_to(
+          world,
+          class: "world-icon-container hover:underline",
         ) do
-          @accessible_worlds.each do |world|
-            link_to(
-              world,
-              class: "world-icon-container hover:underline",
-            ) do
-              div(class: "relative") do
-                image_tag(
-                  world.page_icon_variant,
-                  class: "world-icon",
-                  data: { size: "sm" },
-                )
-                if @current_user.world_cards.active.exists?(world:)
-                  Components::Button(
-                    element: :div,
-                    variant: :outline,
-                    size: :icon_xs,
-                    class: "bg-muted rounded-full absolute -right-2 -top-2",
-                    data: {
-                      controller: "tippy",
-                      tippy_content_value: "you have a wallet card for #{world.name}!",
-                      action: "click->tippy#show:prevent:stop",
-                    },
-                  ) do
-                    Icon(
-                      "huge/loyalty-card",
-                      class: "text-muted-foreground size-4",
-                    )
-                  end
-                end
-              end
-              span(class: "world-icon-label text-xs") do
-                world.name
-              end
-            end
-          end
-
-          @world_cards_pending_key_creation&.each do |card|
-            world = card.world!
-            link_to(
-              [ card, :key_grant ],
-              class: "world-icon-container hover:underline",
-            ) do
-              div(class: "relative") do
-                image_tag(
-                  world.page_icon_variant,
-                  class: "world-icon opacity-50",
-                  data: { size: "sm" },
-                )
-                div(class: "absolute inset-0 flex items-center justify-center") do
-                  Icon("huge/loyalty-card", class: "size-8 text-white")
-                end
-              end
-              span(class: "world-icon-label text-xs text-muted-foreground") do
-                world.name
-              end
-            end
-          end
-
-          if hotwire_native_app?
-            link_to("/scan_qr_code", class: "world-icon-container hover:underline") do
-              Components::Button(
-                element: :div,
-                variant: :outline,
-                class: "world-icon shadow-none border-dashed",
-                data: { world_icon_size: "sm" },
-              ) do
-                Icon("huge/qr-code", class: "size-7 text-muted-foreground")
-              end
-              span(class: "world-icon-label text-xs text-muted-foreground") do
-                "scan to add friend"
-              end
-            end
-          end
-
-          unless @world_cards_pending_key_creation
-            Components::DevicePassesForm(
-              url: home_path,
-              data: {
-                turbo_frame: :other_worlds,
-              },
+          div(class: "relative") do
+            image_tag(
+              world.page_icon_variant,
+              class: "world-icon",
+              data: { size: "sm" },
             )
+            if @current_user.world_cards.active.exists?(world:)
+              Components::Button(
+                element: :div,
+                variant: :outline,
+                size: :icon_xs,
+                class: "bg-muted rounded-full absolute -right-2 -top-2",
+                data: {
+                  controller: "tippy",
+                  tippy_content_value: "you have a wallet card for #{world.name}!",
+                  action: "click->tippy#show:prevent:stop",
+                },
+              ) do
+                Icon(
+                  "huge/loyalty-card",
+                  class: "text-muted-foreground size-4",
+                )
+              end
+            end
+          end
+          span(class: "world-icon-label text-xs") do
+            world.name
           end
         end
+      end
+
+      @world_cards_pending_key_creation&.each do |card|
+        world = card.world!
+        link_to(
+          [ card, :key_grant ],
+          class: "world-icon-container hover:underline",
+        ) do
+          div(class: "relative") do
+            image_tag(
+              world.page_icon_variant,
+              class: "world-icon opacity-50",
+              data: { size: "sm" },
+            )
+            div(class: "absolute inset-0 flex items-center justify-center") do
+              Icon("huge/loyalty-card", class: "size-8 text-white")
+            end
+          end
+          span(class: "world-icon-label text-xs text-muted-foreground") do
+            world.name
+          end
+        end
+      end
+
+      if hotwire_native_app?
+        link_to("/scan_qr_code", class: "world-icon-container hover:underline") do
+          Components::Button(
+            element: :div,
+            variant: :outline,
+            class: "world-icon shadow-none border-dashed",
+            data: { world_icon_size: "sm" },
+          ) do
+            Icon("huge/qr-code", class: "size-7 text-muted-foreground")
+          end
+          span(class: "world-icon-label text-xs text-muted-foreground") do
+            "scan to add friend"
+          end
+        end
+      end
+
+      unless @world_cards_pending_key_creation
+        Components::DevicePassesForm(
+          url: home_path,
+          data: {
+            turbo_frame: :other_worlds,
+          },
+        )
       end
     end
   end
