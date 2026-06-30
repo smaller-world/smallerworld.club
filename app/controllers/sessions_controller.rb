19bf6fe10ba31ff626d0470c9344e79b9d9b7627
kai <1376678+hulloitskai@users.noreply.github.com>
2026-06-29 21:11:07 -0400

    Auto-fill phone number from query param

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,14 +14,18 @@ class SessionsController < ApplicationController
 
   # == Actions ==
 
-  # GET /session/new
+  # GET /session/new[?phone_number=...]
   def new
     respond_to do |format|
       format.html do
         if authenticated?
           redirect_to(after_authentication_url, notice: "you are already signed in!")
         else
-          verification_request = PhoneNumberVerificationRequest.new
+          phone_number = if (phone_number = params[:phone_number])
+            PhoneNumberVerificationRequest
+              .normalize_value_for(:phone_number, phone_number)
+          end
+          verification_request = PhoneNumberVerificationRequest.new(phone_number:)
           render Views::Sessions::New.new(verification_request:)
         end
       end
