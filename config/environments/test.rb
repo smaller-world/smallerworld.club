28dd812a1719d8040ed44a92458c2d2a1b0753ce
kai <1376678+hulloitskai@users.noreply.github.com>
2026-07-13 19:09:39 -0400

    Graduate to smallerworld.club

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {
     protocol: "https",
-    host: "app.smallerworld.club",
+    host: "smallerworld.club",
   }
 
   # Print deprecation notices to the stderr.
